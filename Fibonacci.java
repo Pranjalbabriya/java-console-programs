15bd8ad2985266546f197e8da654c79a9d200bc2
Pranjalbabriya <babriyapranjall@gmail.com>
2025-11-16 00:19:36 +0100

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Fibonacci{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    //Ask user how many terms of Fibonnaci series to print
+    System.out.println("Enter number of terms: ");
+    int n = sc.nextInt();   //Read user input
+
+    //Initialize first two terms of Fibonacci series
+    for(int i=1;i<=n;i++){
+      System.out.println(a + " ");  //Print current term
+
+      //Calculate next term
+      int next = a + b;
+
+      //Update variables for next iteration
+      a = b;
+      b = next;
+    }
+    sc.close();
+  }
+}
